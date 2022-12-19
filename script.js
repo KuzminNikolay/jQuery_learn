d8b3a91f7dc75aab2460d95390f7c60d6cfd87b9
nikolay_kuzmin <pilot.kuzmin@yandex.ru>
2022-12-19 17:00:06 +0300

    commit 20221219

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 $(function() {
-  $('h1').css('color', 'green');
+  $('#btn_start').click(function() {
+    $('#message')
+      .html('MESSAGE')
+      .css('background-color', 'red')
+      .parent()
+      .css('background-color', 'blue')
+      .width(150)
+      .height(80)
+  });
+
+  $('#btn_reset').click(function() {
+    location.reload();
+  });
 });
